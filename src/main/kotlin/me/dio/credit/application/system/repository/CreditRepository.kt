dd0fa66c06424dc0709778fcd7dd25c252c7ba8d
Daniel-0liver <dtoliverbr@gmail.com>
2024-01-08 01:58:12 +0000

    fix: Tipe long misswrited

--- src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
+++ src/main/kotlin/me/dio/credit/application/system/repository/CreditRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CreditRepository: JpaRepository<Credit, long> {
+interface CreditRepository: JpaRepository<Credit, Long> {
 }
